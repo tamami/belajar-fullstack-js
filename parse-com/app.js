5a87ba7e2d46da217a3a80bebe2310802cd0e6fb
tamami <tamami.oka@gmail.com>
2016-05-25 08:03:57 +0700

    finishing code

--- parse-com/app.js
+++ parse-com/app.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
   Parse.initialize(parseApplicationId, parseJavaScriptKey)
   var Text = Parse.Object.extend('Test')
   var test = new Test()
+
+  $('.btn-save').click(function() {
+    try {
+      var data = JSON.parse($('textarea').val())
+    } catch(e) {
+      alert('Invalid JSON')
+    }
+
+    if(!data) return false
+    test.save(data, {
+      success: function(object) {
+        console.log('Parse.com object is saved: ', object)
+        $('.log').html(JSON.stringify(object,null, 2))
+      },
+      error: function(object) {
+        console.log('Error! Parse.com object is not saved: ', object)
+      }
+    })
+  })
 })
